c1eb25853905416a46052da260f3b7805a82f3a9
eduardohk <eduardohk@users.noreply.github.com>
2016-06-22 18:38:48 -0300

    Adicionado o CRUD UsuarioConta e o UsuarioContaJpaController no padrão singleton.

--- src/Aplicativo/src/main/java/br/jpa/entity/Conta.java
+++ src/Aplicativo/src/main/java/br/jpa/entity/Conta.java
@@ -6,7 +6,9 @@
 package br.jpa.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,14 +16,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author Felipe
+ * @author hideki
  */
 @Entity
 @Table(name = "conta")
@@ -40,20 +44,26 @@ public class Conta implements Serializable {
     @Basic(optional = false)
     @Column(name = "c_id")
     private Integer cId;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 30)
     @Column(name = "c_nome")
     private String cNome;
+    
     @Basic(optional = false)
     @NotNull
     @Column(name = "c_valor")
     private double cValor;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 30)
     @Column(name = "c_gerente")
     private String cGerente;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "conta")
+    private Collection<UsuarioConta> usuarioContaCollection;
 
     public Conta() {
     }
@@ -99,6 +109,15 @@ public class Conta implements Serializable {
 
     public void setCGerente(String cGerente) {
         this.cGerente = cGerente;
+    }
+
+    @XmlTransient
+    public Collection<UsuarioConta> getUsuarioContaCollection() {
+        return usuarioContaCollection;
+    }
+
+    public void setUsuarioContaCollection(Collection<UsuarioConta> usuarioContaCollection) {
+        this.usuarioContaCollection = usuarioContaCollection;
     }
 
     @Override
--- src/Aplicativo/src/main/java/br/jpa/entity/Usuario.java
+++ src/Aplicativo/src/main/java/br/jpa/entity/Usuario.java
@@ -6,16 +6,20 @@
 package br.jpa.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -38,16 +42,21 @@ public class Usuario implements Serializable {
     @Size(min = 1, max = 30)
     @Column(name = "u_nome")
     private String uNome;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 30)
     @Column(name = "u_senha")
     private String uSenha;
+    
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 20)
     @Column(name = "u_celular")
     private String uCelular;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "usuario")
+    private Collection<UsuarioConta> usuarioContaCollection;
 
     public Usuario() {
     }
@@ -84,6 +93,15 @@ public class Usuario implements Serializable {
 
     public void setUCelular(String uCelular) {
         this.uCelular = uCelular;
+    }
+
+    @XmlTransient
+    public Collection<UsuarioConta> getUsuarioContaCollection() {
+        return usuarioContaCollection;
+    }
+
+    public void setUsuarioContaCollection(Collection<UsuarioConta> usuarioContaCollection) {
+        this.usuarioContaCollection = usuarioContaCollection;
     }
 
     @Override
