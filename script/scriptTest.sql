e2bea280b14255c9a651be24004a0215a16604b9
matheusmollon <matheusmollon@hotmail.com>
2016-06-27 22:27:01 -0300

    Delete scriptTest.sql

--- script/scriptTest.sql
+++
@@ -1,9 +0,0 @@
--- Testes para o CRUD produto
-
--- executar estas instruções antes de testar:
-
-insert into usuario values ('Teste','1','43 87239033');
-insert into conta values(100,'Costela',0,false,'Teste');
-insert into usuario_conta values('Teste',100,0);
-
--- fim 
